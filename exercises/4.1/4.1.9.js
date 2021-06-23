6471377671e1a5d83235e8e065663977cc25638b
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-23 19:29:31 -0300

    4.1.9 exercise

--- exercises/4.1/4.1.9.js
+++ exercises/4.1/4.1.9.js
@@ -2,7 +2,7 @@ let firstNumber = Math.floor(Math.random() * (50 - 2 + 1)) + 2;
 let secondNumber = Math.floor(Math.random() * (50 - 2 + 1)) + 2;
 let thirdNumber = Math.floor(Math.random() * (50 - 2 + 1)) + 2;
 
-if (firstNumber%2 === 0 || secondNumber%2 === 0 || thirdNumber === 0) {
+if (firstNumber%2 >= 1 || secondNumber%2 >= 1 || thirdNumber%2 >= 1) {
   console.log('true')
 } else {
   console.log('false')
