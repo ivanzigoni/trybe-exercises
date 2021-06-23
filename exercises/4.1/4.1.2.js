fb5bcbc288bccf307bc21d00b94ded4935a6e71f
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-23 18:29:28 -0300

    4.1.2 exercise

---
+++ exercises/4.1/4.1.2.js
@@ -0,0 +1,10 @@
+let firstNumber = 12;
+let secondNumber = 11;
+
+if (firstNumber > secondNumber) {
+    console.log("first number is bigger than the second");
+} else if (firstNumber === secondNumber) {
+    console.log('both numbers are equal')
+} else if (firstNumber < secondNumber) {
+    console.log('second number is bigger than the first')
+}
