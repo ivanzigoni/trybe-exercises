6d70b661ceb63db0a99d43a543d1762692377dff
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-23 18:42:40 -0300

    4.1.4 exercise

---
+++ exercises/4.1/4.1.4.js
@@ -0,0 +1,9 @@
+let number = 0;
+
+if (Math.sign(number) === 1) {
+    console.log("positive");
+} else if (Math.sign(number) === -1) {
+    console.log("negative");
+} else if (number === 0) {
+    console.log("number is 0")
+}
