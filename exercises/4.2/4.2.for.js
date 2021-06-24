022fc57940ea5308febd5df953163413551b1cae
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-24 15:24:11 -0300

    4.2 for/of loop ex1

--- exercises/4.2/4.2.for.js
+++ exercises/4.2/4.2.for.js
@@ -1,7 +1,15 @@
 //for loop
 
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+/*let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
 for (let i = 0; i < groceryList.length; i += 1) {
     console.log(groceryList[i])
+}*/
+
+//for/of
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let values of names) {
+    console.log(values)
 }
