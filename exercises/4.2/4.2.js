5822eae7957c130f756e5aab9483164a95f94750
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-24 19:05:00 -0300

    4.2 exercise 3

--- exercises/4.2/4.2.js
+++ exercises/4.2/4.2.js
@@ -1,7 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum;
-
+let sum = 0;
+let media;
 for (let i = 0; i < numbers.length; i += 1){
     sum += numbers[i]
 }
+
+media = Math.floor(sum / numbers.length)
+
+
 console.log(sum)
+console.log(media)
