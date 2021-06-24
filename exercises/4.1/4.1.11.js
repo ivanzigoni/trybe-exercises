400e9cc2699a297fe186cc1786c6130f6f528bc4
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-24 11:21:52 -0300

    4.1.11 exercise

---
+++ exercises/4.1/4.1.11.js
@@ -0,0 +1,36 @@
+let salarioBruto = 5000;
+let salarioBase;
+let salarioInss;
+let salarioLiquido;
+
+if (salarioBruto <= 1556.94) {
+    let aliquotaInss = salarioBruto / 100 * 8;
+    salarioBase = salarioBruto - aliquotaInss;
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    let aliquotaInss = salarioBruto / 100 * 9
+    salarioBase = salarioBruto - aliquotaInss;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    let aliquotaInss = salarioBruto / 100 * 11;
+    salarioBase = salarioBruto - aliquotaInss;
+} else if (salarioBruto >= 5189.82) {
+    let aliquotaInss = 570.88;
+    salarioBase = salarioBruto - aliquotaInss;
+}
+
+if (salarioBase <= 1903.98) {
+    salarioBase = salarioLiquido;
+} else if (salarioBase >= 1903.99 && salarioBase <= 2826.85) {
+    let aliquotaIr = salarioBase / 100 * 7.5 - 142.80;
+    salarioLiquido = salarioBase - aliquotaIr;
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+    let aliquotaIr = salarioBase / 100 * 15 - 354.80;
+    salarioLiquido = salarioBase - aliquotaIr;
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+    let aliquotaIr = salarioBase / 100 * 22.5 - 636.13;
+    salarioLiquido = salarioBase - aliquotaIr;
+} else if (salarioBase >= 4664.69) {
+    let aliquotaIr = salarioBase / 100 * 27.5 - 869.36;
+    salarioLiquido = salarioBase - aliquotaIr;
+}
+
+console.log(salarioLiquido)
