44d147dc270d9b5fd3a347249a87407798973d31
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-24 15:02:27 -0300

    4.2 arrays ex2

--- exercises/4.2/4.2.js
+++ exercises/4.2/4.2.js
@@ -1,8 +1,14 @@
 //arrays
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices =  menu[1];
 
 
 
-console.log(menuServices);
+console.log(menuServices);*/
+
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
