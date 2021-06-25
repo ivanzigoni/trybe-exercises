49656bcb56ad5a8c9abe4d05f6a764c677fa638a
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-25 16:11:15 -0300

    4.2 exercise 9

--- exercises/4.2/4.2.js
+++ exercises/4.2/4.2.js
@@ -1,9 +1,10 @@
-let numbers = [500, 9, 2, 19, 70, 8, 700, 4, 35, 27];
-let lowest = numbers[0];
-
-for (let i = 0; i < numbers.length; i += 1) {
-    if ( numbers[i] < lowest ) {
-        lowest = numbers[i]
+let numbers = [];
+let num = 0;
+for (let i = 0; i < 25; i += 1) {
+    if (numbers.length < 25) {
+        numbers.push(num += 1)
     }
 }
-console.log(lowest)
+
+console.log(numbers)
+
