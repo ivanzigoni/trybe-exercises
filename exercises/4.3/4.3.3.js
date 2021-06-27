9ae33345c69b043307624f9aaefae45104fa104e
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-27 19:59:38 -0300

    Exercise 4.3.3 doing later

--- exercises/4.3/4.3.3.js
+++ exercises/4.3/4.3.3.js
@@ -1,19 +1,15 @@
+
 let n = 5;
-let linha = "";
+let linha  = "";
 let blank = "";
 let coluna = "";
 
 for (let i = 0; i < n; i += 1) {
-    for (let k = 0; k < n; k += 1) {
-        if (k < n) {
-            linha += "x";
-            coluna = linha;
-        } else if (k === n - 1) {
-            linha += "*";
-            coluna = linha;
+    for (let k = 0; k < n; k += 1){
+        if (k <= n) {
+            blank += "x";
         }
         
-    }
-        console.log(coluna)
-        colunha = "";
+    }  
+    console.log(blank)
 }
