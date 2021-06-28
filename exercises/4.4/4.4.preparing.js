8b07c0eeef2ca5e899990f186553ea1c8050baff
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-28 15:35:44 -0300

    4.4 function preparing exercise

--- exercises/4.4/4.4.preparing.js
+++ exercises/4.4/4.4.preparing.js
@@ -1,9 +1,26 @@
-let names = {
-    person1: 'João',
-    person2: 'Maria',
-    person3: 'Jorge' 
-  };
+let sum = (a, b) => {
+    return a + b;
+}
 
-  for (let key in names) {
-      console.log("Olá, " + names[key])
-  }
+let subt = (a, b) => {
+    return a - b;
+    
+}
+
+let mult = (a, b) => {
+    return a * b;
+}
+
+let divi = (a, b) => {
+    return a / b;
+}
+
+let modu = (a, b) => {
+    return a%b;
+}
+
+console.log(sum(10, 10))
+console.log(subt(10, 10))
+console.log(mult(10, 10))
+console.log(divi(10, 10))
+console.log(modu(10, 10))
