2eb60e3b38d21f3349196c4cea5a5f9befc77959
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-28 19:31:41 -0300

    4.4.functions exercise 2

--- exercises/4.4/4.4.function.js
+++ exercises/4.4/4.4.function.js
@@ -1,7 +1,17 @@
-let palindrome = (str) => {
-    var re = /[\W_]/g;
-    var lowRegStr = str.toLowerCase().replace(re, '');
-    var reverseStr = lowRegStr.split('').reverse().join(''); 
-    return reverseStr === lowRegStr;
-  }
-  console.log(palindrome("A man, a plan, a canal. Panama"))
+let biggerNumber = () => {
+    let numbers = [];
+    let bigger = 0;
+    for (let i = 0; i < 10; i += 1) {
+        numbers.push(Math.floor(Math.random() * 100))
+    }
+    console.log(numbers)
+    for (let i = 0; i < numbers.length; i += 1){
+        if (numbers[i] > bigger) {
+            bigger = numbers[i]
+        }
+    }
+    console.log(bigger)
+}
+
+biggerNumber()
+
---
+++ exercises/4.4/teste.js
@@ -0,0 +1,11 @@
+let numbers = [1, 2, 3, 4, 5, 6, 500, 432, 231, 435, 56, 765]
+let bigger = 2;
+
+
+     if (numbers.length > 1) {
+         bigger = 123
+     }
+
+
+
+console.log(bigger)
