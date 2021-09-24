1756fb6535aba8a0a5d6e19c395305775c9e2f18
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-09-24 15:57:12 -0300

    pqpppp

--- exercises/15.1/preparing/testes-react/src/App.test.js
+++ exercises/15.1/preparing/testes-react/src/App.test.js
@@ -1,24 +1,45 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('Acha input com label Email', () => {
-  const { getByLabelText } = render(<App />);
-  const linkElement = getByLabelText('Email');
-  expect(linkElement).toBeInTheDocument();
-  expect(linkElement.type).toBe('email')
-});
+// test('Acha input com label Email', () => {
+//   const { getByLabelText } = render(<App />);
+//   const linkElement = getByLabelText('Email');
+//   expect(linkElement).toBeInTheDocument();
+//   expect(linkElement.type).toBe('email')
+// });
 
-test('Testando byRole', () => {
-  const { getAllByRole } = render(<App />);
-  const button = getAllByRole('button');
-  expect(button.length).toBe(2);
-})
+// test('Testando byRole', () => {
+//   const { getAllByRole } = render(<App />);
+//   const button = getAllByRole('button');
+//   expect(button.length).toBe(2);
+// })
 
-test('Testando data-testid', () => {
-  const { getByTestId } = render(<App />);
-  const sendBtn = getByTestId('btn-send');
-  const backBtn = getByTestId('btn-back');
+// test('Testando data-testid', () => {
+//   const { getByTestId } = render(<App />);
+//   const sendBtn = getByTestId('btn-send');
+//   const backBtn = getByTestId('btn-back');
 
-  expect(backBtn).toBeInTheDocument();
-  expect(sendBtn).toBeInTheDocument();
+//   expect(backBtn).toBeInTheDocument();
+//   expect(sendBtn).toBeInTheDocument();
+// })
+
+
+test('Testando fireEvent', () => {
+const { getByTestId, getByLabelText } = render(<App />);
+
+const EMAIL_USER = 'email@email.com';
+
+const emailText = getByTestId('id-email-user');
+expect(emailText).toBeInTheDocument();
+expect(emailText).toHaveTextContent('Valor:')
+
+const btnSend = getByTestId('id-send');
+const inputEmail = getByLabelText('Email')
+fireEvent.change(inputEmail, { target: { value: EMAIL_USER }});
+fireEvent.click(btnSend);
+
+expect(emailText).toHaveTextContent(`Valor: ${EMAIL_USER}`);
+expect(inputEmail).toHaveTextContent('');
+expect(inputEmail).toHaveValue('');
+
 })
